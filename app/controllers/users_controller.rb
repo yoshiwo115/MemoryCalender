7d1c2efdcab8f0dcec229ece47339fa934078f0a
yoshiwo <yoshiwo115@gmail.com>
2020-11-08 02:23:44 +0900

    routesrework

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-  end
-
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
